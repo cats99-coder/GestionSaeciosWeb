88d3d08be0c63f3ce17377b2f77d6e830837b8fd
Cristian Andres Tabares Serna <cristianandrestabaresserna@gmail.com>
2023-03-15 09:33:45 +0100

    prueba

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -4,7 +4,7 @@ const AppLayout = ({ children, tittle, toolbar }) => {
   return (
     <>
       <Header />
-      <main className="px-3 py-2 flex flex-col space-y-3 bg-slate-200 grow">
+      <main className="px-3 py-2 flex flex-col space-y-3 bg-slate-200 grow overflow-hidden">
         <div className="text-blue text-md">{tittle}</div>
         {toolbar}
         {children}
