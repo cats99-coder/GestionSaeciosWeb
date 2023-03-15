26bcdb924818fa176b4f0aebdc16c8200f682e65
Cristian Andres Tabares Serna <cristianandrestabaresserna@gmail.com>
2023-03-15 11:29:23 +0100

    prueba

--- src/pages/Gestión Suministros/Tarifas/Tarifas.jsx
+++ src/pages/Gestión Suministros/Tarifas/Tarifas.jsx
@@ -7,7 +7,7 @@ export function Tarifas() {
         <div className="grid grid-cols-3 gap-x-2 grid-rows-[min-content_minmax(0,1fr)] overflow-hidden">
           <div>Productos</div>
           <div className="text-xl text-blue font-bold">Tarifa Básica</div>
-          <div className="text-xl text-blue font-bold">Tarifa Pro</div>
+          <div className="text-xl text-blue font-bold">Tarifa Premium</div>
           <div className="grid grid-cols-3 gap-x-2 gap-y-3 col-span-3 overflow-y-auto">
             <div className="col-span-3 bg-blue rounded-full w-min px-2 text-white">Frutas</div>
             <div className="bg-white p-2">Tomate</div>
